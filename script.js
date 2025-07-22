63ad87824eb4b3cca4dce4d8cf3fb54881d9270b
Moxh <moxhsharma878@gmail.com>
2025-07-22 17:57:48 +0530

    a,

--- script.js
+++ script.js
@@ -7,13 +7,35 @@ var modal = document.querySelector(".modal");
 
 var closeButton = document.querySelector("#Close");
  
+var btn = document.querySelector("#add-task");
+
 
  task.addEventListener("click", function() {
     modal.style.display = "block";
 });
+
+closeButton.addEventListener("click", function(){
+    modal.style.display = "none";
+})
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-    
+
+function allowDrop(e){
+    e.preventDefault();
+}
+function drag(e){
+    e.dataTransfer.setData("task-card", e.target.id);
+}
+function drop(e){   
+    e.preventDefault();
+    var data = e.dataTransfer.getData("task-card");
+    if(e.target.id === "card-contents"){
+        e.target.appendChild(document.getElementById(data));
+    }
+}
+  
+
